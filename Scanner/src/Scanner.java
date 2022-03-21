8afec5de130e0b13b68595099a60e54f7ac54493
Lim-jeongho <limjh0387@gmail.com>
2022-03-21 22:47:44 +0900

    IMPORT 부분에서 오류발생

---
+++ Scanner/src/Scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Scanner {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int x = in.nextInt();
+		int y = in.nextInt();
+		System.out.printf("%d * %d은 %d 입니다. \n", x, y, x*y);
+	}
+
+}
